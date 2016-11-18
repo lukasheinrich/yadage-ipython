722c6cad840cf885f3da02dc3626c80b533a6415
lukasheinrich <lukas.heinrich@gmail.com>
2016-11-18 21:35:15 +0100

    test notebook

--- yadage.js
+++ yadage.js
@@ -1,5 +1,43 @@
-define({
-    hello: function (what){
-      console.log("hello "+what)
+define('yadage',['vis'],function(vis){
+  return {hello: function(el){
+    console.log(el)
+    var container = document.createElement('div');
+    container.style.height="500px";
+    container.style.width="500px";
+    container.style.border="solid black 1px";
+    el.appendChild(container);
+
+    var DOTstring = 'strict digraph {1->2->3;4->3;}';
+    var parsedData = vis.network.convertDot(DOTstring);
+    var netdata = {
+      nodes: parsedData.nodes,
+      edges: parsedData.edges
     }
+
+    netdata.nodes[0].title = 'hello'
+    netdata.nodes[0].label = 'firstlabel'
+
+    console.log(netdata.nodes[0])
+
+    var net_options = parsedData.options;
+    net_options.layout = {
+        hierarchical: {
+            direction: 'UD',
+            sortMethod: 'directed'
+        }
+    }
+    net_options.interaction = {
+      dragNodes: false,
+      dragView: false,
+      hover: true,
+      selectConnectedEdges: false
+    }
+    var network = new vis.Network(container, netdata, net_options);
+    network.on('click',function(data){
+      console.log('got a network event!!')
+      console.log(data.nodes)
+      var thenodes = network.selectNodes(data.nodes);
+      console.log(thenodes)
+    })
+  }}
 });
--- yadage_widget.py
+++ yadage_widget.py
@@ -6,3 +6,16 @@ class WorkflowWidget(widgets.DOMWidget):
     _view_module = Unicode('yadage').tag(sync=True)
     value = Unicode('{"dag": {"nodes": []}}').tag(sync=True)
     dotstring = Unicode('strict digraph {}').tag(sync = True)
+
+
+import adage.visualize
+import time
+class ViewTracker(object):
+    def __init__(self,widget):
+        self.widget = widget
+    def initialize(self,adageobj):
+        pass
+    def track(self,adageobj):
+        self.widget.dotstring = adage.visualize.colorize_graph_at_time(adageobj.dag,time.time()).to_string() 
+    def finalize(self,adageobj):
+        self.widget.dotstring = adage.visualize.colorize_graph_at_time(adageobj.dag,time.time()).to_string() 
